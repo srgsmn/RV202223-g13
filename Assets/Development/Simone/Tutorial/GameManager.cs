4d19c255ea66f8ee3e1ea12a48e7d89331c94ae7
Simone Siragusa <sira.simon@icloud.com>
2023-02-13 12:15:38 +0100

    Game Manager Updated

--- Assets/Development/Simone/Tutorial/GameManager.cs
+++ Assets/Development/Simone/Tutorial/GameManager.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager Instance; // SINGLETON
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        // SINGLETON
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
