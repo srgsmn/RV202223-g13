0a43d0dbe67b95ce0ba1cf0dbe1ec8b152c10dc0
Simone Siragusa <sira.simon@icloud.com>
2023-02-14 16:51:08 +0100

    Added Mode Frame

---
+++ Assets/Development/Simone/Modes/ModeManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModeManager : MonoBehaviour
+{
+    [Header("UI elements:")]
+    [SerializeField] Image frame;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+
+    private void EventsSubscriber(bool subscribing = true)
+    {
+
+    }
+}
